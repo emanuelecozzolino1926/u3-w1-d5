a30286ff8495696f69550f7f6bd05dbe6807dac2
Emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2025-11-21 10:34:03 +0100

    [UPD] Created main contianer

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "./App.css";
 import NetNavBar from "./Components/NetNavBar";
+import NetContainer from "./Components/NetContainer";
 
 function App() {
   return (
     <>
       <NetNavBar />
+      <NetContainer />
     </>
   );
 }
---
+++ src/Components/NetContainer.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const NetContainer = () => {
+  return (
+    <>
+      <Container fluid className="px-4"></Container>
+    </>
+  );
+};
+
+export default NetContainer;
